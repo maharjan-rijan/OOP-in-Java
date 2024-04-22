a15da6f096f108fd79c3501bcf1714f5687d06f9
Rijan Maharjan <rijanq0@gmail.com>
2024-04-22 07:33:00 +0545

    Modified Assignment

--- Assignment/interface/detail.java
+++ Assignment/interface/detail.java
@@ -3,12 +3,12 @@ package Assignment.Interface;
 public class detail implements Government, LocalLevel {
     public void govt()
     {
-        System.out.println("Government of Nepal");
+        System.out.println("government of Nepal");
     }
 
     public void local()
     {
-        System.out.println("Government of Nepal");
+        System.out.println("Local Level Government of Nepal");
     }
     public static void main(String[] args) {
         detail obj = new detail();
